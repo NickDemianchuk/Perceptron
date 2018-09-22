958c3874e8fe6a48e0e0e6f4267cbe5e140a188c
Mykola Demianchuk <nicksetone@gmail.com>
2018-09-22 10:50:32 -0500

    Added Adaline implementation

---
+++ adaline.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+
+class Adaline(object):
+
+    def __init__(self, lrn_rate=0.01, epochs=50):
+        self.lrn_rate = lrn_rate
+        self.epochs = epochs
+
+    def fit(self, X, y):
+        """ Fit training data.
+
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_samples, n_features]
+            Training vectors, where n_samples is the number of samples and
+            n_features is the number of features.
+        y : array-like, shape = [n_samples]
+            Target values.
+
+        Returns
+        -------
+        self : object
+
+        """
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.epochs):
+            net_input = self.net_input(X)
+            # Please note that the "activation" method has no effect
+            # in the code since it is simply an identity function. We
+            # could write `output = self.net_input(X)` directly instead.
+            # The purpose of the activation is more conceptual, i.e.,
+            # in the case of logistic regression, we could change it to
+            # a sigmoid function to implement a logistic regression classifier.
+            output = self.activation(X)
+            errors = (y - output)
+            self.w_[1:] += self.lrn_rate * X.T.dot(errors)
+            self.w_[0] += self.lrn_rate * errors.sum()
+            cost = (errors ** 2).sum() / 2.0
+            self.cost_.append(cost)
+        return self
+
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, X):
+        """Compute linear activation"""
+        return self.net_input(X)
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.activation(X) >= 0.0, 1, -1)
